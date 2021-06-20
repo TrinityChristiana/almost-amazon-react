8ab55b8d21ee7caef89921ed141a524b0418bc50
Trinity Christiana <trinitycterry@gmail.com>
2021-06-20 18:01:41 -0500

    DATA - Add Notes Data Fixes #31

---
+++ src/sample_sql/notes.sql
@@ -0,0 +1,54 @@
+insert into notes (user_id, author_id, book_id, contents, title, public) values ('0bfc3203-b0cd-4437-bd7f-44232d6f4354', '68be73a8-d9c5-400a-a0eb-c835378a3225', null, 'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.
+
+Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.
+
+Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.', 'et ultrices', false);
+insert into notes (user_id, author_id, book_id, contents, title, public) values ('0bfc3203-b0cd-4437-bd7f-44232d6f4354', '2a82a0d3-2aa7-4ddb-a358-1b81cb55216b', null, 'Fusce consequat. Nulla nisl. Nunc nisl.
+
+Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.', 'morbi', false);
+insert into notes (user_id, author_id, book_id, contents, title, public) values ('0bfc3203-b0cd-4437-bd7f-44232d6f4354', 'd349511f-f597-40ac-bfb6-b7b8aa91142b', null, 'In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.
+
+Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.', 'ac neque duis bibendum', false);
+insert into notes (user_id, author_id, book_id, contents, title, public) values ('0bfc3203-b0cd-4437-bd7f-44232d6f4354', '96ffeb85-315e-47d2-a9a3-f657330d475b', null, 'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', 'tempus semper', false);
+insert into notes (user_id, author_id, book_id, contents, title, public) values ('0bfc3203-b0cd-4437-bd7f-44232d6f4354', '0dbb9a00-a710-47c5-bc42-b73428ec9b36', null, 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.
+
+In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.
+
+Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.', 'odio donec vitae nisi', true);
+insert into notes (user_id, author_id, book_id, contents, title, public) values ('0bfc3203-b0cd-4437-bd7f-44232d6f4354', 'dec081c1-92f8-45e1-b157-8eb81239e70f', null, 'Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.
+
+Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.
+
+Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.', 'duis mattis egestas metus', true);
+insert into notes (user_id, author_id, book_id, contents, title, public) values ('0bfc3203-b0cd-4437-bd7f-44232d6f4354', '109cfd2a-82c0-4c73-b84c-7f1d65464724', null, 'Sed ante. Vivamus tortor. Duis mattis egestas metus.
+
+Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.', 'ultrices mattis', true);
+insert into notes (user_id, author_id, book_id, contents, title, public) values ('0bfc3203-b0cd-4437-bd7f-44232d6f4354', '15240f2e-a1be-4f71-8d0d-06408ff160a8', null, 'Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.', 'viverra diam vitae quam', true);
+insert into notes (user_id, author_id, book_id, contents, title, public) values ('0bfc3203-b0cd-4437-bd7f-44232d6f4354', 'dea6fec1-f748-4430-856e-ac572b850b4f', null, 'Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.
+
+Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.', 'non mattis pulvinar', false);
+insert into notes (user_id, author_id, book_id, contents, title, public) values ('0bfc3203-b0cd-4437-bd7f-44232d6f4354', '2bcb554b-0241-4c80-a23c-111c1f3a3823', null, 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.', 'id ligula suspendisse', false);
+insert into notes (user_id, author_id, book_id, contents, title, public) values ('0bfc3203-b0cd-4437-bd7f-44232d6f4354', null, '7adfb051-6aff-4a3e-b486-c2e487958bec', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', 'praesent blandit lacinia', true);
+insert into notes (user_id, author_id, book_id, contents, title, public) values ('0bfc3203-b0cd-4437-bd7f-44232d6f4354', null, 'dd5e7468-dfb6-4fce-a334-ad1787c50e40', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', 'odio condimentum id luctus', false);
+insert into notes (user_id, author_id, book_id, contents, title, public) values ('0bfc3203-b0cd-4437-bd7f-44232d6f4354', null, '25b9cb25-2e03-47f2-b42a-1aeae7253f1c', 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'sapien cum sociis natoque penatibus', true);
+insert into notes (user_id, author_id, book_id, contents, title, public) values ('0bfc3203-b0cd-4437-bd7f-44232d6f4354', null, 'da90e248-eebb-4f8b-91e9-a9034ce822f6', 'Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.
+
+Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.
+
+Phasellus in felis. Donec semper sapien a libero. Nam dui.', 'amet sapien dignissim vestibulum vestibulum', false);
+insert into notes (user_id, author_id, book_id, contents, title, public) values ('0bfc3203-b0cd-4437-bd7f-44232d6f4354', null, '25b9cb25-2e03-47f2-b42a-1aeae7253f1c', 'In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.', 'a nibh in', false);
+insert into notes (user_id, author_id, book_id, contents, title, public) values ('0bfc3203-b0cd-4437-bd7f-44232d6f4354', null, '3faca61e-5af9-4d41-8886-500a1d05e239', 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.', 'risus', false);
+insert into notes (user_id, author_id, book_id, contents, title, public) values ('0bfc3203-b0cd-4437-bd7f-44232d6f4354', null, 'ebb95f19-09f4-4642-8bdf-ae0101edf1ba', 'Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.
+
+Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.
+
+Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.', 'convallis nulla neque libero convallis', false);
+insert into notes (user_id, author_id, book_id, contents, title, public) values ('0bfc3203-b0cd-4437-bd7f-44232d6f4354', null, 'fee9991a-6e61-43c3-b84d-ebe5b8b299b7', 'Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.
+
+Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.
+
+Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.', 'nec dui luctus rutrum nulla', false);
+insert into notes (user_id, author_id, book_id, contents, title, public) values ('0bfc3203-b0cd-4437-bd7f-44232d6f4354', null, 'bfba4ad8-dee3-4bc1-bafb-334772db90f6', 'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.
+
+Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.', 'sapien non mi integer', true);
+insert into notes (user_id, author_id, book_id, contents, title, public) values ('0bfc3203-b0cd-4437-bd7f-44232d6f4354', null, '25b9cb25-2e03-47f2-b42a-1aeae7253f1c', 'Sed ante. Vivamus tortor. Duis mattis egestas metus.', 'praesent', false);
